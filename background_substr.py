1825903bd50df2fa8ff29976140517a20308b84c
AlexRyzhickov <makseljoinb@gmail.com>
2022-05-11 10:46:54 +0300

    adding without comments

--- background_substr.py
+++ background_substr.py
@@ -15,18 +15,6 @@ FRAMES_COUNT = len(paths)
 
 start_time = time.time()
 
-lines = []
-
-
-def drawlines(img1, lines):
-    r, c, _ = img1.shape
-    for r in lines:
-        color = (0, 0, 0)
-        x0, y0 = map(int, [0, -r[2] / r[1]])
-        x1, y1 = map(int, [c, -(r[2] + r[0] * c) / r[1]])
-        img1 = cv.line(img1, (x0, y0), (x1, y1), color, 1)
-    return img1
-
 
 class Camera():
     def __init__(self, frame_width, frame_height, f, size_px):
@@ -57,37 +45,13 @@ c2.translation = np.array([[-58], [16], [27]])
 
 c2.extrinsic_matrix = np.concatenate((c2.rotation, c2.translation), axis=1)
 c2.p = np.dot(c2.intrinsic_matrix, c2.extrinsic_matrix)
-# print(c2.extrinsic_matrix)
 
-# print(c1.p)
-# print(c2.p)
-
-# a1 = np.copy(c1.p[:2])
-# b1 = np.copy(c2.p[:2])
-
-# print(a1)
-# print(b1)
-
-# A = np.concatenate((a1, b1), axis=0)
-
-# print(a)
-
-# exit(0)
 
 T = (c2.translation - c1.translation).reshape(3)
 T_cross = np.array([[0, - T[2], T[1]], [T[2], 0, - T[0]], [-T[1], T[0], 0]])
 E = np.dot(c1.rotation, np.dot(T_cross, np.transpose(c2.rotation)))
 F = np.dot(np.linalg.inv(np.transpose(c1.intrinsic_matrix)), np.dot(E, np.linalg.inv(c2.intrinsic_matrix)))
 
-# lines = np.dot(F, np.array([[1, 2, 3], [1, 2, 3], [1, 2, 3]]))
-
-# print(F)
-
-# line = np.dot(F, np.array([45, 100, 1]))
-# print(line)
-# s = np.dot(p1.reshape((1, 3)), np.dot(F, p2))[0]
-# print(s)
-# lines1 = np.dot(F, pts1_final.transpose())
 
 
 backSub = [cv.createBackgroundSubtractorKNN(), cv.createBackgroundSubtractorKNN(), cv.createBackgroundSubtractorKNN()]
@@ -162,12 +126,8 @@ def processingFrame(frame, backSubKNN, pos):
         ones = np.ones((len(centers), 1))
         pts1_final = np.append(pts1, ones, axis=1)
         return pts1_final
-        # return np.dot(F, pts1_final.transpose())
     else:
         return None
-        # img3 = drawlines(frames[1], lines.transpose(), lines)
-        # lines = np.dot(F, pts1_final.transpose())
-
 
 isEnd = False
 points = []
@@ -198,58 +158,13 @@ while True:
             for point2 in camera_points2:
                 dst = float(np.dot(np.dot(point1, F), point2.transpose()))
                 if abs(round(dst, 1)) == 0.0:
-                    a = np.array([[point1[0], point1[1]]])
-                    b = np.array([[point2[0], point2[1]]])
+                    point_frame_1 = np.array([[point1[0], point1[1]]])
+                    point_frame_2 = np.array([[point2[0], point2[1]]])
 
-                    p = cv.triangulatePoints(c1.p, c2.p, a.T, b.T)
+                    p = cv.triangulatePoints(c1.p, c2.p, point_frame_1.T, point_frame_2.T)
                     # however, homgeneous point is returned
                     p /= p[3]
-                    # print(a, b)
                     print(p.T)
-                    # print('Projected point from openCV:', p.T)
-
-                    # print(a)
-                    # print(a.shape)
-                    # x1 = np.array([[274.128, 624.409]])
-                    # x2 = np.array([[239.571, 533.568]])
-                    # print(x1)
-                    # print(x1.shape)
-                    #
-                    # point_4d_hom = cv.triangulatePoints(c1.p, c2.p, a, b)
-                    # good_pts_mask = np.where(point_4d_hom[3] != 0)[0]
-                    # point_4d = point_4d_hom / point_4d_hom[3]
-                    #
-                    # print("___________")
-                    # print("image 1", a)
-                    # print("image 2", b)
-                    # print("Result", point_4d)
-                    # print(c1.p)
-                    # print(c2.p)
-                    # print("Check point1", np.dot(c1.p, point_4d_hom))
-                    # print("Check point2", np.dot(c2.p, point_4d_hom))
-                    # print("Angle", math.atan((point_4d[1] - 16) / (point_4d[0])) * 180 / math.pi)
-                    # print("___________")
-
-                    # a = np.array([[6, 2, -5], [3, 3, -2], [7, 5, -3]])
-                    # b = np.array([point1[0], point1[1], point2[1], point2[1]])
-                    # x = np.linalg.solve(A, b)
-
-                    # print(x)
-                    # good_pts_mask2 = np.where(x[3] != 0)[0]
-                    # x = x / x[3]
-                    # print(x)
-
-                    # print("image 1", point1)
-                    # print("image 2", point2)
-                    # n1 = np.dot(c1.p, x)
-                    # n2 = np.dot(c2.p, x)
-                    # print("Check point1", n1 / n1[2])
-                    # print("Check point2", n2 / n2[2])
-
-
-                    # print("The value is:", abs(round(dst, 1)) == 0.0)
-                    # a = int(point1[0])
-                    # b = int(point1[1])
 
                     cv.circle(frames[0], (int(point1[0]), int(point1[1])), 4, (255, 0, 0), -1)
                     cv.circle(frames[1], (int(point2[0]), int(point2[1])), 4, (255, 0, 0), -1)
@@ -258,27 +173,6 @@ while True:
 
     for i, frame in enumerate(frames):
         cv.imshow(camera_names[i], frame)
-
-    # if len(points) == 2 and points[0] is not None and points[1] is not None:
-    # print(points[0])
-    # print(points[1])
-    # print(F.shape)
-    # a = np.array([[1,2,3]])
-    # print(np.dot(a, F))
-
-    # dst = np.dot(np.dot(points[0],F), points[1].transpose())[0][0]
-    # print("The value is:", abs(round(dst, 1)) == 0.0)
-    #
-    # if not abs(round(dst, 1)) == 0.0:
-    #     print(dst)
-
-    # print()
-
-    # points.clear()
-    # drawlines(drawlines(frame,))
-    # img3 = drawlines(frames[0], lines.transpose(), lines)
-    # print(lines)
-    # cv.imshow("sdfd", img3)
 
     keyboard = cv.waitKey(30)
     if keyboard == 'q' or keyboard == 27:

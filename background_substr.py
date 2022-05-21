300678027fc11d22f5d09c48a0fe76c4589eb19c
AlexRyzhickov <makseljoinb@gmail.com>
2022-05-21 19:30:54 +0300

    adding processing points on frames

--- background_substr.py
+++ background_substr.py
@@ -1,92 +1,39 @@
 from __future__ import print_function
 import cv2 as cv
-import argparse
 import numpy as np
-import imutils
 import time
 import math
-
 from configuration.configurator import get_cameras_configurations
-from processing.processing import processingFrame
-
-# paths = ["./data/10_08_12/941_075632_0_tl.avi", "./data/10_08_12/941_075632_1_tc.avi", "./data/10_08_12/941_075632_0_tr.avi"]
-# paths = ["./data/28_01_14/675_100834_0_tl.avi", "./data/28_01_14/675_100834_0_tc.avi", "./data/28_01_14/675_100834_0_tr.avi"]
-# camera_names = ["Left Camera", "Center Camera", "Right Camera"]
-# paths = ["./data/synthetic_data/left.avi", "./data/synthetic_data/right.avi"]
-# camera_names = ["Left Camera", "Right Camera"]
-
-# paths = ["./data/unity/movie_016.mp4", "./data/unity/movie_017.mp4"]
-# camera_names = ["Left Camera", "Right Camera"]
-
-# paths = ["./data/unity_data_1/movie_021.mp4", "./data/unity_data_1/movie_022.mp4"]
-paths = ["./data/unity_data_1/movie_022.mp4", "./data/unity_data_1/movie_023.mp4"]
-#
-# camera_names = ["Left Camera", "Right Camera"]
-
-FRAMES_COUNT = len(paths)
+from processing.processing import processingFrame, processing_points_on_image
+import camera.fundamental_matrix as fm
 
 np.set_printoptions(suppress=True)
 my_file = open("some.txt", "w")
 
 start_time = time.time()
 
-import camera.camera as cm
-import camera.fundamental_matrix as fm
-
-# c1 = cm.Camera(1024, 640, 40e-3, 22e-6, -58, 16, -13, 5, 10)
-# c2 = cm.Camera(1024, 640, 40e-3, 22e-6, -58, 16, 27, 5, -10)
-
-
-# c1 = cm.Camera(1920, 1080, 3.507403e-3, 0.00000375, 0, 18, 0, 0, 0)
-# c1 = cm.Camera(1920, 1080, 3.507403e-3, 0.00000375, -16, 18, -15, 0, 0)
-# c2 = cm.Camera(1920, 1080, 3.507403e-3, 0.00000375, -16, 18, 15, 0, 0)
-
 paths, camera_names, cameras = get_cameras_configurations("./data/unity_data_1/configurations_two_cameras.json")
+FRAMES_COUNT = len(paths)
 
-c1 = cameras[0]
-c2 = cameras[1]
+# c1 = cameras[0]
+# c2 = cameras[1]
+# c3 = cameras[2]
 
-# c1 = cm.Camera(1920, 1080, 3.507403e-3, 0.00000375, -16, 18, -15, 5, -10)
-# c2 = cm.Camera(1920, 1080, 3.507403e-3, 0.00000375, -16, 18, 15, 5, 10)
+# F = fm.create_fundamental_matrix(camera1=c1, camera2=c2)
+# F2 = fm.create_fundamental_matrix(camera1=c1, camera2=c3)
 
-# print(c1.p_matrix)
-# print(c2.p_matrix)
+# f_matrix_list = [F, F2]
+# p_matrix_list = [c1.p_matrix, c2.p_matrix, c3.p_matrix]
+# p_matrix_list = [c1.p_matrix, c2.p_matrix]
 
-# c1.p_matrix = np.array([[9.84807753e-01, 0.00000000e+00, -1.73648178e-01, 5.48614234e+01],
-#                         [1.51344359e-02, 9.96194698e-01, 8.58316512e-02, -1.39455064e+01],
-#                         [1.72987394e-01, -8.71557427e-02, 9.81060262e-01, 2.41815441e+01]])
-#
-# c2.p_matrix = np.array([[9.84807753e-01, 0.00000000e+00, 1.73648178e-01, 5.24303489e+01],
-#                         [-1.51344359e-02, 9.96194698e-01, 8.58316512e-02, -1.91343670e+01],
-#                         [-1.72987394e-01, -8.71557427e-02, 9.81060262e-01, -3.51274040e+01]])
+f_matrix_list = []
+p_matrix_list = []
 
-# c1.p_matrix = np.array([[9.84807753e-01, 0.00000000e+00, -1.73648178e-01, 2.73090648e+00],
-#                         [1.51344359e-02, 9.96194698e-01, 8.58316512e-02, -1.07641317e+01],
-#                         [1.72987394e-01, -8.71557427e-02, 9.81060262e-01, 6.05448232e+01]])
-#
-# c2.p_matrix = np.array([[9.84807753e-01, 0.00000000e+00, -1.73648178e-01, -3.66614036e+01],
-#                         [1.51344359e-02, 9.96194698e-01, 8.58316512e-02, -1.13695092e+01],
-#                         [1.72987394e-01, -8.71557427e-02, 9.81060262e-01, 5.36253275e+01]])
+for i in range(1, len(cameras)):
+    f_matrix_list.append(fm.create_fundamental_matrix(camera1=cameras[0], camera2=cameras[i]))
 
-# print(c1.rotation_matrix)
-# print(c2.rotation_matrix)
-
-
-# c1 = cm.Camera(1920, 1080, 3.507403e-3, 0.00000375, -16, 18, -15, 5, 10)
-# c2 = cm.Camera(1920, 1080, 3.507403e-3, 0.00000375, -16, 18, 15, 5, -10)
-#
-# print(c1.intrinsic_matrix)
-# print(c2.intrinsic_matrix)
-#
-# c1 = cm.Camera(1920, 1080, 941, 1, -16, 18, -15, 5, 10)
-# c2 = cm.Camera(1920, 1080, 941, 1, -16, 18, 15, 5, -10)
-#
-# print(c1.intrinsic_matrix)
-# print(c2.intrinsic_matrix)
-
-# exit(0)
-
-F = fm.create_fundamental_matrix(camera1=c1, camera2=c2)
+for camera in cameras:
+    p_matrix_list.append(camera.p_matrix)
 
 backSub = [cv.createBackgroundSubtractorKNN(), cv.createBackgroundSubtractorKNN(), cv.createBackgroundSubtractorKNN()]
 
@@ -117,7 +64,7 @@ while True:
 
     frame_number = captures[0].get(cv.CAP_PROP_POS_FRAMES)
 
-    # if frame_number < 1380:
+    # if frame_number < 553:
     #     continue
 
     for i, frame in enumerate(frames):
@@ -125,56 +72,60 @@ while True:
                               horizon_line_upper_limit)
         points.append(pts)
 
-    points_size = len(points)
-    for i in range(points_size):
-        camera_points = points[i]
-    camera_points1 = points[0]
-    camera_points2 = points[1]
+    # points_size = len(points)
+    # for i in range(points_size):
+    #     camera_points = points[i]
+    # camera_points1 = points[0]
+    # camera_points2 = points[1]
+    # camera_points3 = points[2]
+    #
+    # if camera_points1 is not None and camera_points3 is not None:
+    #     for point1 in camera_points1:
+    #         for point2 in camera_points3:
+    #             dst = float(np.dot(np.dot(point1, F), point2.transpose()))
+    #             if abs(round(dst, 1)) == 0.0:
+    #                 point_frame_1 = np.array([[point1[0], point1[1]]])
+    #                 point_frame_2 = np.array([[point2[0], point2[1]]])
+    #
+    #                 # print(point_frame_1.shape)
+    #                 # print(point_frame_2.shape)
+    #
+    #                 # point_frame_1 = np.array([[float(point1[0]), float(point1[1])]])
+    #                 # point_frame_2 = np.array([[float(point2[0]), float(point2[1])]])
+    #
+    #                 p = cv.triangulatePoints(c1.p_matrix, c3.p_matrix, point_frame_1.T, point_frame_2.T)
+    #                 # however, homgeneous point is returned
+    #                 p /= p[3]
+    #                 new_p = p.T[0]
+    #                 print("frame ", frame_number, new_p)
+    #                 # print(p.T)
+    #
+    #                 # print(c1.p_matrix)
+    #                 # print(c2.p_matrix)
+    #
+    #                 # term = str(p.T) + " Frame: " + str(int(frame_number)) + " Angle: " + str(
+    #                 #     (math.atan((p[1] - 16) / (p[0])) * 180 / math.pi)) + " Distance " + \
+    #                 #        str(math.sqrt(math.pow(-new_p[0] - (-58), 2) + math.pow(-new_p[1] - (16), 2) + math.pow(
+    #                 #            new_p[2] - (-13), 2))) + "\n"
+    #                 # my_file.write(term)
+    #
+    #                 # print(c1.p_matrix)
+    #                 # print(c2.p_matrix)
+    #                 # print(point_frame_1)
+    #                 # print(point_frame_2)
+    #
+    #                 cv.circle(frames[0], (int(point1[0]), int(point1[1])), 4, (255, 0, 0), -1)
+    #                 cv.circle(frames[2], (int(point2[0]), int(point2[1])), 4, (255, 0, 0), -1)
 
-    if camera_points1 is not None and camera_points2 is not None:
-        for point1 in camera_points1:
-            for point2 in camera_points2:
-                dst = float(np.dot(np.dot(point1, F), point2.transpose()))
-                if abs(round(dst, 1)) == 0.0:
-                    point_frame_1 = np.array([[point1[0], point1[1]]])
-                    point_frame_2 = np.array([[point2[0], point2[1]]])
 
-                    # print(point_frame_1.shape)
-                    # print(point_frame_2.shape)
-
-                    # point_frame_1 = np.array([[float(point1[0]), float(point1[1])]])
-                    # point_frame_2 = np.array([[float(point2[0]), float(point2[1])]])
-
-                    p = cv.triangulatePoints(c1.p_matrix, c2.p_matrix, point_frame_1.T, point_frame_2.T)
-                    # however, homgeneous point is returned
-                    p /= p[3]
-                    new_p = p.T[0]
-                    print("frame ", frame_number, new_p)
-                    # print(p.T)
-
-                    # print(c1.p_matrix)
-                    # print(c2.p_matrix)
-
-                    term = str(p.T) + " Frame: " + str(int(frame_number)) + " Angle: " + str(
-                        (math.atan((p[1] - 16) / (p[0])) * 180 / math.pi)) + " Distance " + \
-                           str(math.sqrt(math.pow(-new_p[0] - (-58), 2) + math.pow(-new_p[1] - (16), 2) + math.pow(
-                               new_p[2] - (-13), 2))) + "\n"
-                    my_file.write(term)
-
-                    # print(c1.p_matrix)
-                    # print(c2.p_matrix)
-                    # print(point_frame_1)
-                    # print(point_frame_2)
-
-                    cv.circle(frames[0], (int(point1[0]), int(point1[1])), 4, (255, 0, 0), -1)
-                    cv.circle(frames[1], (int(point2[0]), int(point2[1])), 4, (255, 0, 0), -1)
+    processing_points_on_image(points, 0, f_matrix_list, p_matrix_list, frame_number, frames)
 
     points.clear()
 
     for i, frame in enumerate(frames):
         cv.imshow(camera_names[i], frame)
 
-    keyboard = cv.waitKey(30)
+    keyboard = cv.waitKey(1)
     if keyboard == 'q' or keyboard == 27:
         break
 
--- processing/processing.py
+++ processing/processing.py
@@ -35,7 +35,7 @@ def processingFrame(frame,
             horizontal_size = abs(right[0] - left[0])
             vertical_size = abs(top[1] - bottom[1])
 
-            if not horizontal_size / vertical_size > 4.5:
+            if not horizontal_size / vertical_size > 6:
                 m00, m01, m10, is_not_zero = get_moments(c)
                 if is_not_zero:
                     cX = int(m10 / m00)
@@ -71,7 +71,7 @@ def processingFrame(frame,
         pts1_final = np.append(pts1, ones, axis=1)
         return pts1_final
     else:
-        return None
+        return []
 
 
 def get_moments(c):
@@ -80,3 +80,110 @@ def get_moments(c):
     m01 = M["m01"]
     m10 = M["m10"]
     return m00, m01, m10, m00 != 0
+
+
+class Pair:
+    def __init__(self, point_position_on_main_camera, point_position_on_another_camera, dst, camera_number):
+        self.point_position_on_main_camera = point_position_on_main_camera
+        self.point_position_on_another_camera = point_position_on_another_camera
+        self.dst = dst
+        self.camera_number = camera_number
+
+
+def increment_frequency(checker_dict, key):
+    value = checker_dict.get(key)
+    if value is None:
+        checker_dict[key] = 1
+    else:
+        checker_dict[key] = value + 1
+
+
+# def processing_points_on_image(points_on_main_camera, points_on_another_cameras, f_matrix_list, count_cameras):
+#     pairs = {}
+#     checker_dict = {}
+#     for i, point_on_main_camera in enumerate(points_on_main_camera):
+#         for camera_number, points in enumerate(points_on_another_cameras):
+#             best_pair = Pair(0, 0, 0, 0)
+#             min_dst = 1
+#             for j, point in enumerate(points):
+#                 dst = float(np.dot(np.dot(point_on_main_camera, f_matrix_list[camera_number]), point.transpose()))
+#                 if abs(round(dst, 1)) == 0.0:
+#                     if abs(dst) < min_dst:
+#                         min_dst = dst
+#                         best_pair = Pair(i, j, dst, camera_number)
+#             if not min_dst == 1:
+#                 key = (best_pair.point_position_on_another_camera, camera_number)
+#                 pair = pairs.get(key)
+#                 if pair is None:
+#                     pairs[key] = best_pair
+#                     increment_frequency(checker_dict=checker_dict, key=best_pair.point_position_on_main_camera)
+#                 else:
+#                     if pair.dst > best_pair.dst:
+#                         pairs[key] = best_pair
+#     values = pairs.values()
+
+def triangulate_nviews(P, ip):
+    """
+    Triangulate a point visible in n camera views.
+    P is a list of camera projection matrices.
+    ip is a list of homogenised image points. eg [ [x, y, 1], [x, y, 1] ], OR,
+    ip is a 2d array - shape nx3 - [ [x, y, 1], [x, y, 1] ]
+    len of ip must be the same as len of P
+    """
+    if not len(ip) == len(P):
+        raise ValueError('Number of points and number of cameras not equal.')
+    n = len(P)
+    M = np.zeros([3 * n, 4 + n])
+    for i, (x, p) in enumerate(zip(ip, P)):
+        M[3 * i:3 * i + 3, :4] = p
+        M[3 * i:3 * i + 3, 4 + i] = -x
+    V = np.linalg.svd(M)[-1]
+    X = V[-1, :4]
+    return X / X[3]
+
+
+def processing_points_on_image(points, main_camera_position, f_matrix_list, p_matrix_list, frame_number, frames):
+    main_camera_points = points[main_camera_position]
+
+    if len(main_camera_points) == 0: return
+
+    camera_points = []
+    for i in range(len(points)):
+        if i != main_camera_position:
+            if len(points[i]) > 0:
+                camera_points.append(points[i])
+            else:
+                return
+
+    for main_point in main_camera_points:
+        point_set = []
+        for i, points2 in enumerate(camera_points):
+            min_dst = 1
+            best_point = np.array([[0, 0, 1]])
+            for point in points2:
+                dst = float(np.dot(np.dot(main_point, f_matrix_list[i]), point.transpose()))
+                if abs(dst) < abs(min_dst):
+                    min_dst = dst
+                    best_point[0][0] = point[0]
+                    best_point[0][1] = point[1]
+            if not min_dst == 1:
+                point_set.append(best_point)
+        if len(point_set) == len(points) - 1:
+            point_set.insert(0, np.array([[main_point[0], main_point[1], 1]]))
+            p = triangulate_nviews(p_matrix_list, point_set)
+            # print('________')
+            print("frame ", frame_number, p)
+
+            # point_frame_1 = np.array([[float(point_set[0][0][0]), float(point_set[0][0][0])]])
+            # point_frame_2 = np.array([[float(point_set[1][0][0]), float(point_set[1][0][0])]])
+            #
+            # p = cv.triangulatePoints(p_matrix_list[0], p_matrix_list[1], point_frame_1.T, point_frame_2.T)
+            # # however, homgeneous point is returned
+            # p /= p[3]
+            # new_p = p.T[0]
+            # print("frame ", frame_number, new_p)
+            # print('________')
+
+            for j, frame in enumerate(frames):
+                cv.circle(frame, (int(point_set[j][0][0]), int(point_set[j][0][1])), 4, (255, 0, 0), -1)
+

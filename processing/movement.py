23de85fc0b8d37fba6b78087ed6d5d3611707290
AlexRyzhickov <makseljoinb@gmail.com>
2022-05-22 21:48:31 +0300

    delete minus in velocity calculating

--- processing/movement.py
+++ processing/movement.py
@@ -4,5 +4,5 @@ def calculate_speed(coordinate_vector_curr, coordinate_vector_prev, frame_offset
     difference_vector = (coordinate_vector_curr - coordinate_vector_prev)
     print("______________________")
     print("Frame: ", frame_number, ", velocity calculate for last ", (frame_offset * frame_duration_s), " s")
-    print("Velocity Vector: ", -difference_vector / (frame_offset * frame_duration_s))
+    print("Velocity Vector: ", difference_vector / (frame_offset * frame_duration_s))
     print("______________________")
